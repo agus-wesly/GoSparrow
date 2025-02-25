1548f1e5b6bd28129533a21ec9cdf1c939c71113
aguswesly <agustchannel@gmail.com>
2025-02-25 20:32:34 +0700

    feat : add ascii header

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"example/hello/pkg/terminal"
 	"example/hello/pkg/tiktok"
 	"example/hello/pkg/twitter"
+	"fmt"
+
+	"github.com/mgutz/ansi"
 )
 
 const (
@@ -16,12 +19,13 @@ var foo []string
 var DEBUG bool = false
 
 func main() {
+    promptHeader()
 	prompt := terminal.Select{
 		Opts:    []string{TWITTER, TIKTOK},
 		Message: "Choose Social Media Mode",
 	}
 	var res int
-    err := prompt.Ask(&res)
+	err := prompt.Ask(&res)
 	if err != nil {
 		panic(err)
 	}
@@ -31,7 +35,19 @@ func main() {
 		t := twitter.Tweet{}
 		t.Begin()
 	} else if selectedSocialMedia == TIKTOK {
-        t := tiktok.Tiktok{}
-        t.Begin()
+		t := tiktok.Tiktok{}
+		t.Begin()
 	}
+}
+
+func promptHeader() {
+	fmt.Println(ansi.LightCyan)
+	fmt.Println(` _________         ________                                             `)
+	fmt.Println(` __  ____/_____    __  ___/_____________ _______________________      __`)
+	fmt.Println(` _  / __ _  __ \   _____ \___  __ \  __ ` + "`" + `/_  ___/_  ___/  __ \_ | /| / /`)
+	fmt.Println(` / /_/ / / /_/ /   ____/ /__  /_/ / /_/ /_  /   _  /   / /_/ /_ |/ |/ / `)
+	fmt.Println(` \____/  \____/    /____/ _  .___/\__,_/ /_/    /_/    \____/____/|__/  `)
+	fmt.Println(`                            /_/                                           `)
+	fmt.Println(`                                            Created with 🤍 by : Wesly  `)
+	fmt.Println(ansi.Reset)
 }
